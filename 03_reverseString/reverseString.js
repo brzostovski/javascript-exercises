9f0c0d5ca38b94efa319e39f902acf5c9fb8d672
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-08 09:08:44 +0200

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+  let stringToReverse = input.split(``);
+  let reversedString = [];
+  let originalStringLen = input.length;
+  for (let i = 1; i <= originalStringLen; i++) {
+    reversedString[i - 1] = stringToReverse[originalStringLen - i];
+  }
+  return reversedString.join(``);
 };
 
 // Do not edit below this line
