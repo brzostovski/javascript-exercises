0109711a2a6a55f907627577bf178c30d0e888cf
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-08 15:15:36 +0200

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+  if (firstNumber < 0 ||
+    secondNumber < 0 ||
+    typeof firstNumber !== `number` ||
+    typeof secondNumber !== `number`) {
+    return `ERROR`;
+  }
+  let start = 0;
+  let end = 0;
+  let sumInBetween = 0;
+  if (firstNumber < secondNumber) {
+    start = firstNumber;
+    end = secondNumber;
+  } else {
+    start = secondNumber;
+    end = firstNumber;
+  }
+  let i = start;
+  do {
+    sumInBetween += i;
+    i++;
+  } while (i <= end);
+  return sumInBetween;
 };
 
 // Do not edit below this line
