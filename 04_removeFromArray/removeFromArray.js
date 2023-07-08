1654db73faf9b1e00708941bc947ad4259f5d813
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-08 14:29:21 +0200

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArr, ...itemsToRemove) {
+  for (const item of itemsToRemove) {
+    for (const argument of givenArr) {
+      if (argument === item) {
+        let index = givenArr.indexOf(argument);
+        givenArr.splice(index, 1);
+      }
+    }
+  }
+  return givenArr;
 };
 
 // Do not edit below this line
