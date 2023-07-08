16519d61a1191f10da538e29dcbd1ac1c5cac8c7
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-08 08:37:40 +0200

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, repeatsNumber) {
+  let repeatedString = ``;
+  if (repeatsNumber < 0) {
+    return 'ERROR';
+  }
+  for (let i = 0; i < repeatsNumber; i++) {
+    repeatedString += stringToRepeat;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
