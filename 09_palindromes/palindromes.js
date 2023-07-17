c7ed844dbb8f534a2aa9ef6ae4c4746c30e062ba
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-17 17:30:59 +0200

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  strippedStr = (str.replace(/\W/g, "")).toLowerCase();
+  arrOfStr = strippedStr.split('');
+  let lastIndex = arrOfStr.length - 1;
+  let compareArr = [];
+  for (let i = 0; i <= lastIndex; i++) {
+    compareArr[lastIndex - i] = arrOfStr[i];
+  }
+  const compareStr = (compareArr.toString()).replaceAll(',', '');
+  if (compareStr === strippedStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
