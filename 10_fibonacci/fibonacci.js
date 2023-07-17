a1a886b2d8f4d3c9f3fa1b45ea320db4f33246be
brzostovski <137798050+brzostovski@users.noreply.github.com>
2023-07-17 17:57:17 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  const nthFibon = parseInt(n);
+  if (nthFibon <= 0) {
+    return 'OOPS';
+  }
+  let arrOfFibon = [];
+  arrOfFibon[0] = 0;
+  arrOfFibon[1] = 1;
+  for (let i = 2; i <= nthFibon; i++) {
+    arrOfFibon[i] = arrOfFibon[i - 1] + arrOfFibon [i - 2];
+  }
+  return arrOfFibon[nthFibon];
 };
 
 // Do not edit below this line
